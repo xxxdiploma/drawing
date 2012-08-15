6acc4da35ef24c385ef334777f2fd55e9b8a081e
xxxdiploma <xxxdiploma@gmail.com>
2012-08-15 13:38:05 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Drawing::Application.config.secret_token = '7ebaf62decb5984768cd92b629a736e627fe357ad348f118f6f033ad6a321b2de5a082a4d053d285a3e0d24cc6b566b78faff051958d66866bdca62135142860'
