4189a1984155b194a74b65f1c8ba16f1385fda90
xxxdiploma <xxxdiploma@gmail.com>
2012-08-22 13:57:03 +0400

    Done with sign in/out

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -36,6 +36,31 @@ describe "Users" do
         end.should change(User, :count).by(1)
       end
     end
+  end
 
+  describe "sign in/out" do
+
+    describe "failure" do
+      it "should not sign a user in" do
+        visit signin_path
+        fill_in I18n.t('sessions.new.email'), :with => ""
+        fill_in I18n.t('sessions.new.password'), :with => ""
+        click_button
+        response.should have_selector("div.flash.error", :content => I18n.t('flash.error.login') )
+      end
+    end
+
+    describe "success" do
+      it "should sign a user in and out" do
+        user = FactoryGirl.create(:user)
+        visit signin_path
+        fill_in I18n.t('sessions.new.email'), :with => user.email
+        fill_in I18n.t('sessions.new.password'), :with => user.password
+        click_button
+        controller.should be_signed_in
+        click_link I18n.t('links.sign_out')
+        controller.should_not be_signed_in
+      end
+    end
   end
 end
