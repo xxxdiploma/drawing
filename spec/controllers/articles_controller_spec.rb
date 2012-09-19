db16a562d2c8b62c2258b419087d439c8f068e00
xxxdiploma <xxxdiploma@gmail.com>
2012-09-19 22:25:00 +0400

    Updated tests

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -4,14 +4,13 @@ describe ArticlesController do
   render_views
 
   describe "GET 'index'" do
-
     before(:each) do
       @user = FactoryGirl.create(:user)
       @admin = FactoryGirl.create(:user, :email => "admin@example.com", :admin => true)
       @article = FactoryGirl.create(:article, :user => @admin, :content => "Sample text")
     end
 
-    describe "as a non-signed-in user" do
+    describe "for non-signed-in user" do
       it "should deny access" do
         get :index
         response.should redirect_to(signin_path)
@@ -19,83 +18,78 @@ describe ArticlesController do
     end
 
     describe "for signed-in users" do
-      before(:each) do
-        test_sign_in(@user)
-      end
-
-      it "should be successful" do
-        get :index
-        response.should be_success
-      end
-
-      it "should have the right title" do
-        get :index
-        response.should have_selector("title", :content => I18n.t('titles.articles') )
-      end
-
-      it "should show the articles" do
-        mp1 = FactoryGirl.create(:article, :user => @admin, :content => "Sample text 1")
-        mp2 = FactoryGirl.create(:article, :user => @admin, :content => "Sample text 2")
-        get :index
-        response.should have_selector("div.articles.content", :content => mp1.content)
-        response.should have_selector("div.articles.content", :content => mp2.content)
-      end
-
-      it "should paginate articles" do
-
-        articles = [@article]
-
-        3.times do
-          articles << FactoryGirl.create(:article, :user => @admin, :content => "Sample text")
+      describe "as an non-admin user" do
+        before(:each) do
+          test_sign_in(@user)
         end
 
-        get :index
-        response.should have_selector("div.pagination")
-        response.should have_selector("span.disabled", :content => I18n.t('will_paginate.previous_label'))
-        response.should have_selector("a", :href => "/articles?page=2", :content => "2")
-        response.should have_selector("a", :href => "/articles?page=2", :content => I18n.t('will_paginate.next_label'))
-      end
-    end
+        it "should be successful" do
+          get :index
+          response.should be_success
+        end
 
-    describe "as an non-admin user" do
-      before(:each) do
-        test_sign_in(@user)
+        it "should have the right title" do
+          get :index
+          response.should have_selector("title", :content => I18n.t('titles.articles'))
+        end
+
+        it "should show the articles" do
+          article1 = FactoryGirl.create(:article, :user => @admin, :content => "Sample text 1")
+          article2 = FactoryGirl.create(:article, :user => @admin, :content => "Sample text 2")
+          get :index
+          response.should have_selector("div.articles.content", :content => article1.content)
+          response.should have_selector("div.articles.content", :content => article2.content)
+        end
+
+        it "should paginate articles" do
+          articles = []
+
+          5.times do
+            articles << FactoryGirl.create(:article, :user => @admin, :content => "Sample text")
+          end
+
+          get :index
+          response.should have_selector("div.pagination")
+          response.should have_selector("span.disabled", :content => I18n.t('will_paginate.previous_label'))
+          response.should have_selector("a", :href => "/articles?page=2", :content => "2")
+          response.should have_selector("a", :href => "/articles?page=2", :content => I18n.t('will_paginate.next_label'))
+        end
+
+        it "should not have 'new article' link" do
+          get :index
+          response.should_not have_selector("a", :content => I18n.t('articles.index.new'))
+        end
+
+        it "should not have 'edit' links" do
+          get :index
+          response.should_not have_selector("a", :content => I18n.t('articles.article.edit'))
+        end
+
+        it "should not have 'delete' links" do
+          get :index
+          response.should_not have_selector("a", :content => I18n.t('articles.article.delete'))
+        end
       end
 
-      it "should not have 'new article' link" do
-        get :index
-        response.should_not have_selector("a", :content => I18n.t('articles.index.new'))
-      end
+      describe "as an admin user" do
+        before(:each) do
+          test_sign_in(@admin)
+        end
 
-      it "should not have 'edit' links" do
-        get :index
-        response.should_not have_selector("a", :content => I18n.t('articles.article.edit'))
-      end
+        it "should have 'new article' link" do
+          get :index
+          response.should have_selector("a", :content => I18n.t('articles.index.new'))
+        end
 
-      it "should not have 'delete' links" do
-        get :index
-        response.should_not have_selector("a", :content => I18n.t('articles.article.delete'))
-      end
-    end
+        it "should have 'edit' links" do
+          get :index
+          response.should have_selector("a", :content => I18n.t('articles.article.edit'))
+        end
 
-    describe "as an admin user" do
-      before(:each) do
-        test_sign_in(@admin)
-      end
-
-      it "should have 'new article' link" do
-        get :index
-        response.should have_selector("a", :content => I18n.t('articles.index.new'))
-      end
-
-      it "should have 'edit' links" do
-        get :index
-        response.should have_selector("a", :content => I18n.t('articles.article.edit'))
-      end
-
-      it "should have 'delete' links" do
-        get :index
-        response.should have_selector("a", :content => I18n.t('articles.article.delete'))
+        it "should have 'delete' links" do
+          get :index
+          response.should have_selector("a", :content => I18n.t('articles.article.delete'))
+        end
       end
     end
   end
@@ -103,40 +97,41 @@ describe ArticlesController do
   ##############################################
 
   describe "GET 'new'" do
-
     before(:each) do
       @user = FactoryGirl.create(:user)
       @admin = FactoryGirl.create(:user, :email => "admin@example.com", :admin => true)
     end
 
-    describe "as a non-signed-in user" do
+    describe "for non-signed-in user" do
       it "should deny access" do
         get :new
         response.should redirect_to(signin_path)
       end
     end
 
-    describe "as a non-admin user" do
-      it "should protect the page" do
-        test_sign_in(@user)
-        get :new
-        response.should redirect_to(articles_path)
-      end
-    end
-
-    describe "as an admin user" do
-      before(:each) do
-        test_sign_in(@admin)
+    describe "for signed-in user" do
+      describe "as a non-admin user" do
+        it "should protect the page" do
+          test_sign_in(@user)
+          get :new
+          response.should redirect_to(articles_path)
+        end
       end
 
-      it "should have the right title" do
-        get :new
-        response.should have_selector("title", :content => I18n.t('titles.publication') )
-      end
+      describe "as an admin user" do
+        before(:each) do
+          test_sign_in(@admin)
+        end
 
-      it "should have a textarea" do
-        get :new
-        response.should have_selector("textarea[name='article[content]']")
+        it "should have the right title" do
+          get :new
+          response.should have_selector("title", :content => I18n.t('titles.publication'))
+        end
+
+        it "should have a textarea" do
+          get :new
+          response.should have_selector("textarea[name='article[content]']")
+        end
       end
     end
   end
@@ -144,42 +139,43 @@ describe ArticlesController do
   ##############################################
 
   describe "POST 'create'" do
-
     before(:each) do
       @user = FactoryGirl.create(:user)
       @admin = FactoryGirl.create(:user, :email => "admin@example.com", :admin => true)
     end
 
-    describe "as a non-signed-in user" do
+    describe "for non-signed-in user" do
       it "should deny access" do
         post :create, :article => { :content => "Sample text" }
         response.should redirect_to(signin_path)
       end
     end
 
-    describe "as a non-admin user" do
-      it "should protect the action" do
-        test_sign_in(@user)
-        post :create, :article => { :content => "Sample text" }
-        response.should redirect_to(articles_path)
-      end
-    end
-
-    describe "as an admin user" do
-      before(:each) do
-        test_sign_in(@admin)
-      end
-
-      it "should not create a new article" do
-        lambda do
-          post :create, :article => { :content => "" }
-        end.should_not change(Article, :count)
-      end
-
-      it "should create a new article" do
-        lambda do
+    describe "for signed-in user" do
+      describe "as a non-admin user" do
+        it "should protect the action" do
+          test_sign_in(@user)
           post :create, :article => { :content => "Sample text" }
-        end.should change(Article, :count).by(1)
+          response.should redirect_to(articles_path)
+        end
+      end
+
+      describe "as an admin user" do
+        before(:each) do
+          test_sign_in(@admin)
+        end
+
+        it "should not create a new article" do
+          lambda do
+            post :create, :article => { :content => "" }
+          end.should_not change(Article, :count)
+        end
+
+        it "should create a new article" do
+          lambda do
+            post :create, :article => { :content => "Sample text" }
+          end.should change(Article, :count).by(1)
+        end
       end
     end
   end
@@ -187,42 +183,43 @@ describe ArticlesController do
   ##############################################
 
   describe "DELETE 'destroy'" do
-
     before(:each) do
       @user = FactoryGirl.create(:user)
       @admin = FactoryGirl.create(:user, :email => "admin@example.com", :admin => true)
       @article = FactoryGirl.create(:article, :user => @admin, :content => "Sample text")
     end
 
-    describe "as a non-signed-in user" do
+    describe "for non-signed-in user" do
       it "should deny access" do
         delete :destroy, :id => @article
         response.should redirect_to(signin_path)
       end
     end
 
-    describe "as a non-admin user" do
-      it "should protect the action" do
-        test_sign_in(@user)
-        delete :destroy, :id => @article
-        response.should redirect_to(articles_path)
-      end
-    end
-
-    describe "as an admin user" do
-      before(:each) do
-        test_sign_in(@admin)
-      end
-
-      it "should destroy the article" do
-        lambda do
+    describe "for signed-in user" do
+      describe "as a non-admin user" do
+        it "should protect the action" do
+          test_sign_in(@user)
           delete :destroy, :id => @article
-        end.should change(Article, :count).by(-1)
+          response.should redirect_to(articles_path)
+        end
       end
 
-      it "should redirect to the articles page" do
-        delete :destroy, :id => @article
-        response.should redirect_to(articles_path)
+      describe "as an admin user" do
+        before(:each) do
+          test_sign_in(@admin)
+        end
+
+        it "should destroy the article" do
+          lambda do
+            delete :destroy, :id => @article
+          end.should change(Article, :count).by(-1)
+        end
+
+        it "should redirect to the articles page" do
+          delete :destroy, :id => @article
+          response.should redirect_to(articles_path)
+        end
       end
     end
   end
@@ -230,41 +227,107 @@ describe ArticlesController do
   ##############################################
 
   describe "GET 'edit'" do
-
     before(:each) do
       @user = FactoryGirl.create(:user)
       @admin = FactoryGirl.create(:user, :email => "admin@example.com", :admin => true)
       @article = FactoryGirl.create(:article, :user => @admin, :content => "Sample text")
     end
 
-    describe "as a non-signed-in user" do
+    describe "for non-signed-in user" do
       it "should protect the action" do
         get :edit, :id => @article
         response.should redirect_to(signin_path)
       end
     end
 
-    describe "as a non-admin user" do
-      it "should protect the action" do
-        test_sign_in(@user)
-        get :edit, :id => @article
-        response.should redirect_to(articles_path)
+    describe "for signed-in user" do
+      describe "as a non-admin user" do
+        it "should protect the action" do
+          test_sign_in(@user)
+          get :edit, :id => @article
+          response.should redirect_to(articles_path)
+        end
+      end
+
+      describe "as an admin user" do
+        before(:each) do
+          test_sign_in(@admin)
+        end
+
+        it "should be successful" do
+          get :edit, :id => @article
+          response.should be_success
+        end
+
+        it "should have the right title" do
+          get :edit, :id => @article
+          response.should have_selector("title", :content => I18n.t('titles.article_edit'))
+        end
+      end
+    end
+  end
+
+  ##############################################
+
+  describe "PUT 'update'" do
+    before(:each) do
+      @user = FactoryGirl.create(:user)
+      @admin = FactoryGirl.create(:user, :email => "admin@example.com", :admin => true)
+      @article = FactoryGirl.create(:article, :user => @admin, :content => "Sample text")
+      @attr = { :content => "New text" }      
+    end
+
+    describe "for non-signed-in user" do
+      it "should deny access" do
+        put :update, :id => @article, :article => @attr
+        response.should redirect_to(signin_path)
       end
     end
 
-    describe "as an admin user" do
-      before(:each) do
-        test_sign_in(@admin)
+    describe "for signed-in users" do
+      describe "as non-admin user" do 
+        it "should redirect to articles path" do
+          test_sign_in(@user)
+          put :update, :id => @article, :article => @attr
+          response.should redirect_to(articles_path)
+        end
       end
 
-      it "should be successful" do
-        get :edit, :id => @article
-        response.should be_success
-      end
+      describe "as an admin user" do
+        describe "failure" do
+          before(:each) do
+            test_sign_in(@admin)
+            @empty_attr = { :content => "" }
+          end
 
-      it "should have the right title" do
-        get :edit, :id => @article
-        response.should have_selector("title", :content => I18n.t('titles.article_edit') )
+          it "should render the 'edit' page" do
+            put :update, :id => @article, :article => @empty_attr
+            response.should render_template('edit')
+          end
+
+          it "should have the right title" do
+            put :update, :id => @article, :article => @empty_attr
+            response.should have_selector("title", :content => I18n.t('titles.article_edit'))
+          end                
+        end
+
+        describe "success" do
+          before(:each) do
+            test_sign_in(@admin)
+            @new_attr = { :content => "another text" }
+          end
+
+          it "should change the attributes" do
+            put :update, :id => @article, :article => @new_attr
+            @article.reload
+            @article.content.should == @new_attr[:content]
+          end
+
+          it "should redirect to the storage show page" do
+            put :update, :id => @article, :article => @new_attr
+            response.should redirect_to(articles_path)
+          end
+        end        
       end
     end
   end
--- spec/controllers/storages_controller_spec.rb
+++ spec/controllers/storages_controller_spec.rb
@@ -4,14 +4,13 @@ describe StoragesController do
   render_views
 
   describe "GET 'index'" do
-
     before(:each) do
       @user = FactoryGirl.create(:user)
       @admin = FactoryGirl.create(:user, :email => "admin@example.com", :admin => true)
       @file = FactoryGirl.create(:storage, :user => @admin, :title => "title")
     end
 
-    describe "as a non-signed-in user" do
+    describe "for non-signed-in user" do
       it "should deny access" do
         get :index
         response.should redirect_to(signin_path)
@@ -19,42 +18,44 @@ describe StoragesController do
     end
 
     describe "for signed-in users" do
-      before(:each) do
-        test_sign_in(@user)
-      end
-
-      it "should be successful" do
-        get :index
-        response.should be_success
-      end
-
-      it "should have the right title" do
-        get :index
-        response.should have_selector("title", :content => I18n.t('titles.storages') )
-      end
-
-      it "should show the storages" do
-        get :index
-        response.should have_selector("li", :content => @file.title)
-      end
-
-      it "should paginate storages" do
-        storages = []
-
-        30.times do
-          storages << FactoryGirl.create(:storage, :user => @admin, :title => "title")
+      describe "as non-admin user" do
+        before(:each) do
+          test_sign_in(@user)
         end
 
-        get :index
-        response.should have_selector("div.pagination")
-        response.should have_selector("span.disabled", :content => I18n.t('will_paginate.previous_label'))
-        response.should have_selector("a", :href => "/storages?page=2", :content => "2")
-        response.should have_selector("a", :href => "/storages?page=2", :content => I18n.t('will_paginate.next_label'))
-      end
+        it "should be successful" do
+          get :index
+          response.should be_success
+        end
 
-      it "should not have 'delete' links" do
-        get :index
-        response.should_not have_selector("a", :content => I18n.t('storages.storage.delete'))
+        it "should have the right title" do
+          get :index
+          response.should have_selector("title", :content => I18n.t('titles.storages') )
+        end
+
+        it "should show the storages" do
+          get :index
+          response.should have_selector("li", :content => @file.title)
+        end
+
+        it "should paginate storages" do
+          storages = []
+
+          30.times do
+            storages << FactoryGirl.create(:storage, :user => @admin, :title => "title")
+          end
+
+          get :index
+          response.should have_selector("div.pagination")
+          response.should have_selector("span.disabled", :content => I18n.t('will_paginate.previous_label'))
+          response.should have_selector("a", :href => "/storages?page=2", :content => "2")
+          response.should have_selector("a", :href => "/storages?page=2", :content => I18n.t('will_paginate.next_label'))
+        end
+
+        it "should not have 'delete' links" do
+          get :index
+          response.should_not have_selector("a", :content => I18n.t('storages.storage.delete'))
+        end
       end
     end
 
@@ -75,14 +76,13 @@ describe StoragesController do
   ##############################################
 
   describe "GET 'show'" do
-
     before(:each) do
       @user = FactoryGirl.create(:user)
       @admin = FactoryGirl.create(:user, :email => "admin@example.com", :admin => true)
       @file = FactoryGirl.create(:storage, :user => @admin, :title => "title")
     end
 
-    describe "as a non-signed-in user" do
+    describe "for non-signed-in user" do
       it "should deny access" do
         get :show, :id => @file
         response.should redirect_to(signin_path)
@@ -90,56 +90,57 @@ describe StoragesController do
     end
 
     describe 'for signed-in users' do
-      before(:each) do
-        test_sign_in(@user)
+      describe "as non-admin user" do      
+        before(:each) do
+          test_sign_in(@user)
+        end
+
+        it "should be successful" do
+          get :show, :id => @file
+          response.should be_success
+        end
+
+        it "should find the right storage" do
+          get :show, :id => @file
+          assigns(:storage).should == @file
+        end
+     
+        it "should have the right title" do
+          get :show, :id => @file
+          response.should have_selector("title", :content => @file.title)
+        end
+
+        it "should not have an edit button" do
+          get :show, :id => @file
+          response.should_not have_selector("a", :content => I18n.t('storages.show.edit'))  
+        end
+
+        it "should have an download button" do
+          get :show, :id => @file
+          response.should have_selector("a", :content => I18n.t('storages.show.download'))  
+        end
       end
 
-      it "should be successful" do
-        get :show, :id => @file
-        response.should be_success
-      end
-
-      it "should find the right storage" do
-        get :show, :id => @file
-        assigns(:storage).should == @file
-      end
-   
-      it "should have the right title" do
-        get :show, :id => @file
-        response.should have_selector("title", :content => @file.title)
-      end
-
-      it "should not have an edit button" do
-        get :show, :id => @file
-        response.should_not have_selector("a", :content => I18n.t('storages.show.edit'))  
-      end
-
-      it "should have an download button" do
-        get :show, :id => @file
-        response.should have_selector("a", :content => I18n.t('storages.show.download'))  
-      end
+      describe "as an admin user" do
+        it "should have an edit button" do
+          test_sign_in(@admin)
+          get :show, :id => @file
+          response.should have_selector("a", :content => I18n.t('storages.show.edit'))  
+        end
+      end 
     end
-
-    describe "as an admin user" do
-      it "should have an edit button" do
-        test_sign_in(@admin)
-        get :show, :id => @file
-        response.should have_selector("a", :content => I18n.t('storages.show.edit'))  
-      end
-    end 
   end
 
   ##############################################
 
   describe "GET 'edit'" do
-
     before(:each) do
       @user = FactoryGirl.create(:user)
       @admin = FactoryGirl.create(:user, :email => "admin@example.com", :admin => true)
       @file = FactoryGirl.create(:storage, :user => @admin, :title => "title")
     end    
 
-    describe "as a non-signed-in user" do
+    describe "for non-signed-in user" do
       it "should deny access" do
         get :edit, :id => @file
         response.should redirect_to(signin_path)
@@ -147,33 +148,33 @@ describe StoragesController do
     end
 
     describe "for signed-in users" do
-      it "should redirect to storages path" do
-        test_sign_in(@user)
-        get :edit, :id => @file
-        response.should redirect_to(storages_path)
+      describe "as non-admin user" do 
+        it "should redirect to storages path" do
+          test_sign_in(@user)
+          get :edit, :id => @file
+          response.should redirect_to(storages_path)
+        end
       end
+
+      describe "as an admin user" do
+        it "should be successful" do
+          test_sign_in(@admin)
+          get :edit, :id => @file
+          response.should be_success
+        end
+
+        it "should have a right title" do
+          test_sign_in(@admin)
+          get :edit, :id => @file
+          response.should have_selector("title", :content => I18n.t('titles.file_edit')) 
+        end
+      end 
     end
-
-    describe "as an admin user" do
-      it "should be successful" do
-        test_sign_in(@admin)
-        get :edit, :id => @file
-        response.should be_success
-      end
-
-      it "should have a right title" do
-        test_sign_in(@admin)
-        get :edit, :id => @file
-        response.should have_selector("title", :content => I18n.t('titles.file_edit')) 
-      end
-    end 
-
   end
 
   ##############################################
 
   describe "PUT 'update'" do
-
     before(:each) do
       @user = FactoryGirl.create(:user)
       @admin = FactoryGirl.create(:user, :email => "admin@example.com", :admin => true)
@@ -181,7 +182,7 @@ describe StoragesController do
       @attr = { :title => "new title", :description => "new description" }
     end    
 
-    describe "as a non-signed-in user" do
+    describe "for non-signed-in user" do
       it "should deny access" do
         put :update, :id => @file, :storage => @attr
         response.should redirect_to(signin_path)
@@ -189,49 +190,53 @@ describe StoragesController do
     end
 
     describe "for signed-in users" do
-      it "should redirect to storages path" do
-        test_sign_in(@user)
-        put :update, :id => @file, :storage => @attr
-        response.should redirect_to(storages_path)
+      describe "as non-admin user" do 
+        it "should redirect to storages path" do
+          test_sign_in(@user)
+          put :update, :id => @file, :storage => @attr
+          response.should redirect_to(storages_path)
+        end
       end
+
+      describe "as an admin user" do
+        describe "failure" do
+          before(:each) do
+            test_sign_in(@admin)
+            @empty_attr = { :title => "" }
+          end
+
+          it "should render the 'edit' page" do
+            put :update, :id => @file, :storage => @empty_attr
+            response.should render_template('edit')
+          end
+
+          it "should have the right title" do
+            put :update, :id => @file, :storage => @empty_attr
+            response.should have_selector("title", :content => I18n.t('titles.file_edit'))
+          end
+        end
+
+        describe "success" do
+          before(:each) do
+            test_sign_in(@admin)
+            @new_attr = { :title => "another title" }
+          end
+
+          it "should change the attributes" do
+            put :update, :id => @file, :storage => @new_attr
+            @file.reload
+            @file.title.should  == @new_attr[:title]
+          end
+
+          it "should redirect to the storage show page" do
+            put :update, :id => @file, :storage => @new_attr
+            response.should redirect_to(storage_path(@file))
+          end
+        end        
+      end      
     end   
-
-    describe "failure" do
-
-      before(:each) do
-        test_sign_in(@admin)
-        @empty_attr = { :title => "" }
-      end
-
-      it "should render the 'edit' page" do
-        put :update, :id => @file, :storage => @empty_attr
-        response.should render_template('edit')
-      end
-
-      it "should have the right title" do
-        put :update, :id => @file, :storage => @empty_attr
-        response.should have_selector("title", :content => I18n.t('titles.file_edit'))
-      end
-    end
-
-    describe "success" do
-
-      before(:each) do
-        test_sign_in(@admin)
-        @new_attr = { :title => "another title" }
-      end
-
-      it "should change the attributes" do
-        put :update, :id => @file, :storage => @new_attr
-        @file.reload
-        @file.title.should  == @new_attr[:title]
-      end
-
-      it "should redirect to the storage show page" do
-        put :update, :id => @file, :storage => @new_attr
-        response.should redirect_to(storage_path(@file))
-      end
-    end
   end
+
+  ##############################################
 
 end
